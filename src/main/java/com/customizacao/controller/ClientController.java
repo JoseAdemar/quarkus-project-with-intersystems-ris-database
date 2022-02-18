0e5cd98a1114a6286f08eeb45c14cfd5cfdb6351
grongierisc <guillaume.rongier@intersystems.com>
2022-02-18 11:38:59 +0100

    fix controler and client

--- src/main/java/com/customizacao/controller/ClientController.java
+++ src/main/java/com/customizacao/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.customizacao.controller;
 
 import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,35 +11,32 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 import com.customizacao.entity.Client;
-import com.customizacao.service.ClientService;
+
+import org.jboss.logging.Logger;
 
 @Path("/api/guests")
+@ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ClientController {
 
-private final ClientService clientService;
-	
-	public ClientController(ClientService clientService) {
-		this.clientService = clientService;
-	}
+	private static final Logger LOGGER = Logger.getLogger(ClientController.class.getName());
 
 	@POST
-	public Response createClient(@RequestBody Client client) {
-		clientService.create(client);
-		return Response.status(Status.CREATED).entity(client).build();
+	@Transactional
+	public Response createClient(Client client) {
+		LOGGER.info(client);
+        client.persist();
+        return Response.ok(client).status(201).build();
 	}
 	
 	
 	@GET
-	public Response listAllClients() {
-		List<Client> listClients = clientService.findAll();
-		return Response.status(Status.OK).entity(listClients).build();
+	public List<Client> listAllClients() {
+		return Client.listAll();
+
 	}
 
 }
--- src/main/java/com/customizacao/entity/Client.java
+++ src/main/java/com/customizacao/entity/Client.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -17,22 +18,15 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tbl_client")
-public class Client {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@EqualsAndHashCode.Include
-	private Long id;
+public class Client extends PanacheEntity {
 
 	
-	@Size(max = 50)
-	@Column(name = "first_name")
-	private String firstName;
+	@Column(length = 50, name = "first_name")
+	public String firstName;
 
 	
-	@Size(max = 50)
-	@Column(name = "last_name")
-	private String lastName;
+	@Column(length = 50, name = "last_name")
+	public String lastName;
 	
 }
 	
